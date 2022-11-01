000be00fb079da73ec5cb5769619b25ed07cabb7
salmanpary <salmanpary@gmail.com>
2022-11-01 21:30:33 +0530

    header changes

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 import { HiOutlineLogout } from "react-icons/hi";
 const Header = () => {
   return (
-    <Link href="/transactions">
-      <div className="flex mx-9 bg-primaryblue justify-between items-center px-20 my-5 rounded-2xl py-3">
+    <div className="flex mx-9 bg-primaryblue justify-between items-center px-20 my-5 rounded-2xl py-3">
+        <Link href="/transactions">
         <Image src="/logo.svg" alt="logo" width={160} height={55} />
+    </Link>
+          <Link href='/'>
         <button className="flex bg-grey text-primaryred rounded-lg px-3 py-2 gap-3 text-base font-medium">
           <HiOutlineLogout className="w-6 h-6" />
           <div className="">Logout</div>
         </button>
+          </Link>
       </div>
-    </Link>
   );
 };
 
